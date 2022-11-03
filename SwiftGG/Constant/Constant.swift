7543bdb70d32df617e1d0016922a5acf7e5b538c
stoicer <43617591@qq.com>
2022-11-03 09:04:16 +0800

    登录后数据保存

--- SwiftGG/Constant/Constant.swift
+++ SwiftGG/Constant/Constant.swift
@@ -13,8 +13,53 @@ let kNavgationBarHeight = 44.0
 let kStatusBarH : CGFloat = 20
 
 /**************************  UserDefault用到Key  ***************************/
+let GGUserDefault = UserDefaults.standard
+
 //登录使用到的公钥
 let kLoginPublicKey = "kLoginPublicKey"
 
 //域名信息保存
 let kCompanyInfoKey = "kCompanyInfoKey"
+
+//登录后token
+let kGGToken = "kGGToken"
+
+//登录后保存用户名
+let kGGUserName = "kGGUserName"
+
+//登录后保存头像
+let kGGAvatar = "kGGAvatar"
+
+//登录后保存用户姓名
+let kGGName = "kGGName"
+
+//登录后保存im状态
+let kImState = "kImState"
+
+//分享桌面
+let kFreeDesk = "kFreeDesk"
+
+//im账号
+let kIMAccount = "kIMAccount"
+
+//im密码
+let kIMPwd = "kIMPwd"
+
+//用户是否vip
+let kIsVIP = "kIsVIP"
+
+//昵称列表
+let kNickName_list = "kNickName_list"
+
+//模板列表
+let kTemplate_info = "kTemplate_info"
+
+//系统信息
+let kUserSysInfo = "kUserSysInfo"
+
+//公司信息 - public版本
+let kBranchCompaniesInfo = "kBranchCompaniesInfo"
+
+//权限列表
+let kAuth_list = "kAuth_list"
+
--- SwiftGG/DB/GGDataManager.swift
+++ SwiftGG/DB/GGDataManager.swift
@@ -7,14 +7,7 @@
 
 import Foundation
 
-let kGGToken = "kGGToken"
-let kGGUserName = "kGGUserName"
-let kGGAvatar = "kGGAvatar"
-let kGGName = "kGGName"
-let kImState = "kImState"
-let kFreeDesk = "kFreeDesk"
 
-let userDefault = UserDefaults.standard
 
 class GGDataManager
 {
@@ -28,62 +21,29 @@ class GGDataManager
         let name = dic["name"]
         let imState = dic["jg_im_enable"]
         let takenSystemDesk = dic["taken_sys_desktop"] as? Bool
+        let jgImUserName = dic["jg_im_uname"]
+        let jgImPwd = dic["jg_im_pwd"]
+        let isVip = dic["grade"]
+        let nickNameList = dic["kNickName_list"]
+        let templateInfo = dic["template_info"]
+        let userSysInfo =  dic["sys_props"]
+        let companyInfo = dic["kBranchCompaniesInfo"]
+        let authList = dic["auth_list"]
         
-        
-        userDefault.set(token, forKey: kGGToken)
-        userDefault.set(userName, forKey: kGGUserName)
-        userDefault.set(avatarUrl, forKey: kGGAvatar)
-        userDefault.set(name, forKey: kGGAvatar)
-        userDefault.set(imState, forKey: kImState)
-        userDefault.set(takenSystemDesk, forKey: kImState)
-
-        /*
-        [[NSUserDefaults standardUserDefaults] setObject:[NSNumber numberWithBool:[data[@"taken_sys_desktop"]boolValue]] forKey:kFreeDesk];
-        [[NSUserDefaults standardUserDefaults] setObject:data[@"jg_im_uname"] forKey:kIMAccount];
-        [[NSUserDefaults standardUserDefaults] setObject:data[@"jg_im_pwd"] forKey:kIMPwd];
-        [[NSUserDefaults standardUserDefaults] setObject:[NSString stringWithFormat:@"%@",data[@"grade"]] forKey:kIsVIP];
-        [[NSUserDefaults standardUserDefaults] setObject:data[kNickName_list] forKey:kNickName_list];
-        POTemplateEntity *temp = [POTemplateEntity yy_modelWithJSON:data[@"template_info"]];
-        NSData *tempData = [NSKeyedArchiver archivedDataWithRootObject:temp];
-        [[NSUserDefaults standardUserDefaults] setObject:tempData forKey:kTemplateInfoCache];
-        [[NSUserDefaults standardUserDefaults] synchronize];
-        NSDictionary *dic = data[@"sys_props"];
-        
-        [[NSUserDefaults standardUserDefaults] setObject:[dic deleteNull] forKey:kUserSysInfo];
-        
-        //用户购买或加入的公司（公服版才有用）
-        NSArray *branchCompaniesArr = data[@"branchCompanies"];
-        [[NSUserDefaults standardUserDefaults] setObject:branchCompaniesArr forKey:kBranchCompaniesInfo];
-        
-        //用户认证方式:个人认证或企业认证（公服版才有用）
-        if ([branchCompaniesArr isKindOfClass:[NSArray class]] && branchCompaniesArr.count > 0)
-        {
-            NSDictionary *dict = branchCompaniesArr.firstObject;
-            NSNumber *authenticationModel = dict[@"authenticationModel"];
-            if (authenticationModel)
-            {
-                [[NSUserDefaults standardUserDefaults] setObject:authenticationModel forKey:kPublicAuthenticationMode];
-            }
-            
-            //是否重新发起合同签署 合同认证是否通过 1、通过，2、不通过。不通过时候，需要重新签署
-            NSNumber *contractSignFlag = dict[@"contractPass"];
-            NSString *signName = dict[@"signName"] ? : @"";
-            if ([contractSignFlag integerValue] == 2 && [GetIMAccount isEqualToString:signName])
-            {
-                [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kPublicContractSign];
-            }
-            else
-            {
-                [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kPublicContractSign];
-            }
-        }
-        
-        [[NSUserDefaults standardUserDefaults] synchronize];
-        [PODataManager saveAuthority:data[@"auth_list"]];
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"kDidSaveUserInfo" object:nil];
-        */
-        
-        
+        GGUserDefault.set(token, forKey: kGGToken)
+        GGUserDefault.set(userName, forKey: kGGUserName)
+        GGUserDefault.set(avatarUrl, forKey: kGGAvatar)
+        GGUserDefault.set(name, forKey: kGGAvatar)
+        GGUserDefault.set(imState, forKey: kImState)
+        GGUserDefault.set(takenSystemDesk, forKey: kImState)
+        GGUserDefault.set(jgImUserName, forKey: kIMAccount)
+        GGUserDefault.set(jgImPwd, forKey: kIMPwd)
+        GGUserDefault.set(isVip, forKey: kIsVIP)
+        GGUserDefault.set(nickNameList, forKey: kNickName_list)
+        GGUserDefault.set(templateInfo, forKey: kTemplate_info)
+        GGUserDefault.set(userSysInfo, forKey: kUserSysInfo)
+        GGUserDefault.set(companyInfo, forKey: kBranchCompaniesInfo)
+        GGUserDefault.set(authList, forKey: kAuth_list)
     }
 }
 
